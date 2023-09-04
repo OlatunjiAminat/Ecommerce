fb710938205d1caf91cf064cf82337897f8d8f8a
Aminat Olatunji <olatunjiaminat1990@gmail.com>
2023-09-04 15:23:27 -0700

    start nextjs

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+
+import NavBar from '../components/nav'
+
+function page() {
+  return (
+    <div>
+        <NavBar />
+    </div>
+  )
+}
+
+export default page
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,18 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        "brown": "rgb(216, 125, 74)",
+        "light-brown": "rgb(251, 175, 133)",
+        "black": "rgb(16, 16, 16)",
+        "light-black": "rgb(0, 0, 0)",
+        "pure-white": "rgb(255, 255, 255)",
+        "light-white": "rgb(250, 250, 250)",
+        "deep-white": "rgb(241, 241, 241)",
+      },
+      fontFamily: {
+        abc: ["Manrope"]
+      },
     },
   },
   plugins: [],
